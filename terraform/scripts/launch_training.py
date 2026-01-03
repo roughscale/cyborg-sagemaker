ee73c31bc22f5533db43aeb8e337d0921552933a
Brenton O'Loughlin <brenton.oloughlin@roughscale.com>
2026-01-03 16:46:37 +1100

    seems there is an undocumented limit of 16 hyperparameters in the sagemaker console display.  move environment specific vars to env vars to free up hyperparameter space

--- terraform/scripts/launch_training.py
+++ terraform/scripts/launch_training.py
@@ -261,8 +261,6 @@ class SageMakerTrainingLauncher:
         job_hyperparameters = {
             "algorithm": algorithm,
             "total_steps": str(total_steps),
-            "scenario_name": scenario,
-            "environment_mode": environment_mode,
         }
 
         if seed is not None:
