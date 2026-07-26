7622a943da2f0aec4179b927ab6537b190b64dd0
Brenton O'Loughlin <brenton.oloughlin@roughscale.com>
2026-07-26 11:22:16 +1000

    update DRQN max_grad_norm and n_prev_seq parameters to match original hyperparameters

--- src/training/algorithms/drqn.py
+++ src/training/algorithms/drqn.py
@@ -137,7 +137,7 @@ def train_drqn(env, args, callbacks: List[BaseCallback]) -> Any:
         exploration_fraction=args.exploration_fraction,
         exploration_initial_eps=args.initial_epsilon,
         exploration_final_eps=args.final_epsilon,
-        max_grad_norm=10,  # Gradient clipping
+        max_grad_norm=1.0,  # Gradient clipping (LSTM sensitive to exploding gradients via BPTT)
         tensorboard_log="/opt/ml/output/tensorboard",
         policy_kwargs={"net_arch": net_arch},
         verbose=1,
